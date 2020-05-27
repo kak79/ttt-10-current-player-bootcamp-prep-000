46a53b3ad87a32a77559944ab84e4a580032327c
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-27 14:04:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,11 +3,7 @@
 def turn_count(board)
   count = 0
   board.each do |board_element|
-    if board_element == "X"
-      puts "#{count}"
-    elsif board_element == "O"
-      puts "#{count}"
-    else
+    if board_element == "X" || board_element == "O"
       count += 1
     end
   end
